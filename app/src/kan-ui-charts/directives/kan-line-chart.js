617d23616cb33960a0b288394c167527b2fc3225
Eran Sakal <eran.sakal@kaltura.com>
2015-12-31 14:59:55 +0200

    embed line chart of nvd3 (with sample data)

--- app/src/kan-ui-charts/directives/kan-line-chart.js
+++ app/src/kan-ui-charts/directives/kan-line-chart.js
@@ -1,9 +1,58 @@
 'use strict';
 
+var nv = require('nvd3');
 module.exports =  function()
 {
 
+    function data() {
+        var sin = [],
+            cos = [];
+
+        for (var i = 0; i < 100; i++) {
+            sin.push({x: i, y: Math.sin(i/10)});
+            cos.push({x: i, y: .5 * Math.cos(i/10)});
+        }
+
+        return [
+            {
+                values: sin,
+                key: 'Sine Wave',
+                color: '#ff7f0e'
+            },
+            {
+                values: cos,
+                key: 'Cosine Wave',
+                color: '#2ca02c'
+            }
+        ];
+    }
+
     function linkFunction ($scope, element, attrs) {
+        nv.addGraph(function() {
+            var chart = nv.models.lineChart()
+                .useInteractiveGuideline(true)
+                ;
+
+            chart.xAxis
+                .axisLabel('Time (ms)')
+                .tickFormat(d3.format(',r'))
+            ;
+
+            chart.yAxis
+                .axisLabel('Voltage (v)')
+                .tickFormat(d3.format('.02f'))
+            ;
+
+            d3.select(element.find('svg')[0])
+                .datum(data())
+                .transition().duration(500)
+                .call(chart)
+            ;
+
+            nv.utils.windowResize(chart.update);
+
+            return chart;
+        });
 
 
     }
--- app/vendors-references.js
+++ app/vendors-references.js
@@ -1,10 +1,11 @@
 'use strict';
 
 module.exports = {
-    'angular' : './node_modules/angular/angular.js',
+    'angular': './node_modules/angular/angular.js',
     'lodash': './bower_components/lodash/lodash.js',
-    'angular-animate':'./bower_components/angular-animate/angular-animate.js',
-    'angular-ui-router':'./bower_components/angular-ui-router/release/angular-ui-router.js',
-    'ui-bootstrap-tpls':'./bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
-    'jquery':'./bower_components/jquery/dist/jquery.js'
-};
+    'angular-animate': './bower_components/angular-animate/angular-animate.js',
+    'angular-ui-router': './bower_components/angular-ui-router/release/angular-ui-router.js',
+    'ui-bootstrap-tpls': './bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+    'jquery': './bower_components/jquery/dist/jquery.js',
+    'nvd3': './libs/nvd3/nv.d3.min.js'
+}
