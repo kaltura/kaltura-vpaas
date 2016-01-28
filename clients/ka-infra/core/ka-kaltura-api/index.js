d7f7e11a090a58653d0fdc2d35a9c819d9eb1c09
Eran Sakal <eran.sakal@kaltura.com>
2016-01-28 16:36:17 +0200

    code cleanup

--- clients/ka-infra/core/ka-kaltura-api/index.js
+++ clients/ka-infra/core/ka-kaltura-api/index.js
@@ -1,13 +1,13 @@
 'use strict';
 
 
-var appModule = require('./ka-kaltura-api.module.js');
-appModule.run(require('./ka-kaltura-api.run.js'));
-appModule.config(require('./ka-kaltura-api.config.js'));
+var appModule = require('./ka-kaltura-api.module');
+appModule.run(require('./ka-kaltura-api.run'));
+appModule.config(require('./ka-kaltura-api.config'));
 
-appModule.provider('kaKalturaAPIFacade',require('./services/ka-kaltura-api-facade.provider.js'));
+appModule.provider('kaKalturaAPIFacade',require('./services/ka-kaltura-api-facade.provider'));
 
-appModule.service('kaRequestsHandlerUtils',require('./services/ka-requests-handler-utils.service.js'));
+appModule.service('kaRequestsHandlerUtils',require('./services/ka-requests-handler-utils.service'));
 
-appModule.config(require('./config/handlers/report-service/get-url-for-report-as-csv.config.js'));
-appModule.config(require('./config/handlers/report-service/get-table.config.js'));
+appModule.config(require('./config/handlers/report-service/get-url-for-report-as-csv.config'));
+appModule.config(require('./config/handlers/report-service/get-table.config'));
